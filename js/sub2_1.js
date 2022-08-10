91cf7616b9cc2ad0e6b6237f53b3a5bbfa6a236f
mingyu <108381497+mingyu1128@users.noreply.github.com>
2022-08-10 11:22:48 +0900

    Delete js directory

--- js/sub2_1.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".gnb li").mouseover(function(){
-        $("#sub").stop().slideDown();
-    });
-    $(".gnb li").mouseleave(function(){
-        $("#sub").stop().slideUp();
-    });
-});
